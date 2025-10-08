c4dc68d544d1cfb1052dca78487b2e6145086aff
jayblair-sys <jayblair080@gmail.com>
2025-10-08 12:17:07 +0100

    added code
    code

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public float newX;
+    public float newY;
+
+    public Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newX = Input.GetAxis("Horizontal");
+        newY = Input.GetAxis("Vertical");
+        
+    }
+}
